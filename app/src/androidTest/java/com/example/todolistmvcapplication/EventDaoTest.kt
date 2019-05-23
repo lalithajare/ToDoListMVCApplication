dd8f2684f25104d1ff66937cb8acc268436abc18
lalithajare <hajare.lalit@gmail.com>
2019-05-23 12:27:51 +0530

    Test case written for EventDao Unit

---
+++ app/src/androidTest/java/com/example/todolistmvcapplication/EventDaoTest.kt
@@ -0,0 +1,36 @@
+package com.example.todolistmvcapplication
+
+import com.example.todolistmvcapplication.daos.EventDao
+import com.example.todolistmvcapplication.models.Event
+import com.example.todolistmvcapplication.utils.MyApplication
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
+import org.mockito.Mock;
+import java.util.*
+
+class EventDaoTest {
+    //Non-zero Long return value, to test whether values are inserting
+    @Test
+    fun insertEventTest() {
+        assertNotEquals(
+            MyApplication.getInstance().eventDB!!.eventDao().insertEvent(
+                Event(
+                    0,
+                    "TestEvent",
+                    Date(),
+                    "xyz event is awsome"
+                )
+            ), 0
+        )
+    }
+
+    //Whether the events in DB not empty
+    @Test
+    fun getEventsTest() {
+        System.out.println("Number of events in DB : "+MyApplication.getInstance().eventDB!!.eventDao().getEvents().size)
+        assertNotEquals(
+            MyApplication.getInstance().eventDB!!.eventDao().getEvents().size, 0
+        )
+    }
+}
